9fdfbfb0680727cf5ef7518df1477b9300085f93
Gabriel <gabriel.prevosto@insa-rennes.fr>
2016-01-21 13:43:27 +0100

    Squelette TP

---
+++ TPUnitySquelette/Assets/Scripts/MovementController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+	
+	private Rigidbody body;
+	// TODO
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movement
+		if(Input.GetKey(KeyCode.Z))
+		{
+			// TODO
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			// TODO
+		}
+		// Rotations
+		if(Input.GetKey(KeyCode.Q))
+		{
+			// TODO
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			// TODO
+		}
+	}
+}
