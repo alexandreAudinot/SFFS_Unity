709776fe63b2d81163691e391371825cc0a913d9
Gabriel <gabriel.prevosto@insa-rennes.fr>
2016-01-21 12:57:25 +0100

    Solution finie

---
+++ TPUnity/Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public float porjectileSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject o = GameObject.Instantiate(prefab);
+            o.transform.position = transform.position;
+            o.transform.rotation = transform.rotation;
+            Rigidbody body = o.GetComponent<Rigidbody>();
+            body.AddRelativeForce(0f, 0f, porjectileSpeed, ForceMode.Impulse);
+        }	
+	}
+}
---
+++ TPUnity/Assets/Scripts/TimeOutScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeOutScript : MonoBehaviour {
+
+    public float lifetime = 5;
+
+    private float life;
+
+	// Use this for initialization
+	void Start () {
+        life = lifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        life -= Time.deltaTime;
+        if (life < 0)
+            Destroy(this.gameObject);
+	}
+}
